018ac69e6805727541d97a42e4401ee9dd5966a6
船岡プログラミング教室 <higashifunaoka@gmail.com>
2025-06-25 17:13:27 +0900

    Update HekigaSeisei.java

--- HekigaSeisei.java
+++ HekigaSeisei.java
@@ -49,10 +49,10 @@ public class HekigaSeisei implements ClientTickEvents.EndTick{
         return gyouzentai;
     }
 
-    public void hekigaMinami(ClientWorld pZentai, MinecraftClient pJibun){
-        List<String> gyouzentai = gazouYomitori("hekigaMinami.txt");
+    public void hekigaSakusei(MinecraftClient pJibun, String gazou, int kaishiX, int kaishiZ, int zougenX, int zougenZ){
         BlockPos genzaichi = pJibun.player.getBlockPos();
-        BlockPos kaishi = genzaichi.add(gyouzentai.get(0).length() / 2, gyouzentai.size(), gyouzentai.get(0).length() / 2);
+        List<String> gyouzentai = gazouYomitori(gazou);
+        BlockPos kaishi = genzaichi.add(gyouzentai.get(0).length() / 2 * kaishiX, gyouzentai.size(), gyouzentai.get(0).length() / 2 * kaishiZ);
         for (int tate = 0; tate < gyouzentai.size(); tate++) {
             String ichigyou = gyouzentai.get(tate);
             for (int yoko = 0; yoko < ichigyou.length(); yoko++) {
@@ -60,62 +60,8 @@ public class HekigaSeisei implements ClientTickEvents.EndTick{
                 int narabijun = SUUJI_MOJI_0_9_A_Z_a_z.indexOf(moji);
                 if (narabijun >= 0 && narabijun < Iro62Shurui.IRO_62_SHURUI.length) {
                     Block masume = Iro62Shurui.IRO_62_SHURUI[narabijun];
-                    BlockPos basho = kaishi.add(-yoko, -tate, 0);
-                    pZentai.setBlockState(basho, masume.getDefaultState());
-                }
-            }
-        }
-    }
-
-    public void hekigaNishi(ClientWorld pZentai, MinecraftClient pJibun){
-        List<String> gyouzentai = gazouYomitori("hekigaNishi.txt");
-        BlockPos genzaichi = pJibun.player.getBlockPos();
-        BlockPos kaishi = genzaichi.add(- gyouzentai.get(0).length() / 2, gyouzentai.size(), gyouzentai.get(0).length() / 2);
-        for (int tate = 0; tate < gyouzentai.size(); tate++) {
-            String ichigyou = gyouzentai.get(tate);
-            for (int yoko = 0; yoko < ichigyou.length(); yoko++) {
-                char moji = ichigyou.charAt(yoko);
-                int narabijun = SUUJI_MOJI_0_9_A_Z_a_z.indexOf(moji);
-                if (narabijun >= 0 && narabijun < Iro62Shurui.IRO_62_SHURUI.length) {
-                    Block masume = Iro62Shurui.IRO_62_SHURUI[narabijun];
-                    BlockPos basho = kaishi.add(0, -tate, -yoko);
-                    pZentai.setBlockState(basho, masume.getDefaultState());
-                }
-            }
-        }
-    }
-
-    public void hekigaKita(ClientWorld pZentai, MinecraftClient pJibun){
-        List<String> gyouzentai = gazouYomitori("hekigaKita.txt");
-        BlockPos genzaichi = pJibun.player.getBlockPos();
-        BlockPos kaishi = genzaichi.add(- gyouzentai.get(0).length() / 2, gyouzentai.size(), - gyouzentai.get(0).length() / 2);
-        for (int tate = 0; tate < gyouzentai.size(); tate++) {
-            String ichigyou = gyouzentai.get(tate);
-            for (int yoko = 0; yoko < ichigyou.length(); yoko++) {
-                char moji = ichigyou.charAt(yoko);
-                int narabijun = SUUJI_MOJI_0_9_A_Z_a_z.indexOf(moji);
-                if (narabijun >= 0 && narabijun < Iro62Shurui.IRO_62_SHURUI.length) {
-                    Block masume = Iro62Shurui.IRO_62_SHURUI[narabijun];
-                    BlockPos basho = kaishi.add(yoko, -tate, 0);
-                    pZentai.setBlockState(basho, masume.getDefaultState());
-                }
-            }
-        }
-    }
-
-    public void hekigaHigashi(ClientWorld pZentai, MinecraftClient pJibun){
-        List<String> gyouzentai = gazouYomitori("hekigaHigashi.txt");
-        BlockPos genzaichi = pJibun.player.getBlockPos();
-        BlockPos kaishi = genzaichi.add(gyouzentai.get(0).length() / 2, gyouzentai.size(), - gyouzentai.get(0).length() / 2);
-        for (int tate = 0; tate < gyouzentai.size(); tate++) {
-            String ichigyou = gyouzentai.get(tate);
-            for (int yoko = 0; yoko < ichigyou.length(); yoko++) {
-                char moji = ichigyou.charAt(yoko);
-                int narabijun = SUUJI_MOJI_0_9_A_Z_a_z.indexOf(moji);
-                if (narabijun >= 0 && narabijun < Iro62Shurui.IRO_62_SHURUI.length) {
-                    Block masume = Iro62Shurui.IRO_62_SHURUI[narabijun];
-                    BlockPos basho = kaishi.add(0, -tate, yoko);
-                    pZentai.setBlockState(basho, masume.getDefaultState());
+                    BlockPos basho = kaishi.add(yoko * zougenX, -tate, yoko * zougenZ);
+                    pJibun.world.setBlockState(basho, masume.getDefaultState());
                 }
             }
         }
@@ -123,27 +69,24 @@ public class HekigaSeisei implements ClientTickEvents.EndTick{
 
     @Override
     public void onEndTick(MinecraftClient pJibun){
-        ClientWorld zentai = pJibun.world;
+        final String[] GAZOU_NAMAE = {
+            "hekigaMinami.txt",
+            "hekigaNishi.txt",
+            "hekigaKita.txt",
+            "hekigaHigashi.txt"
+        };
+        final int[][] GAZOU_ICHI = {
+            {  1,  1, -1,  0 },
+            { -1,  1,  0, -1 },
+            { -1, -1,  1,  0 },
+            {  1, -1,  0,  1 }
+        };        
         // マウス左クリック
-        if (zentai != null && pJibun.player != null && pJibun.mouse.wasLeftButtonClicked()){
-            float kakudo = (pJibun.player.getYaw() + 360) % 360;
-            //System.out.println("角度:" + kakudo);
-            // 南
-            if(kakudo < 45 || kakudo > 315){
-                hekigaMinami(zentai, pJibun);
-            }
-            // 西
-            else if (kakudo < 135){
-                hekigaNishi(zentai, pJibun);
-            }
-            // 北
-            else if (kakudo < 225){
-                hekigaKita(zentai, pJibun);
-            }
-            // 東
-            else{
-                hekigaHigashi(zentai, pJibun);
-            }
+        if (pJibun.world != null && pJibun.player != null && pJibun.mouse.wasLeftButtonClicked()){
+            int kakudo = ((int)pJibun.player.getYaw() + 360 + 45) % 360;
+            int hougaku = kakudo / 90;
+            System.out.println("degree (kakudo):" + kakudo);
+            hekigaSakusei(pJibun, GAZOU_NAMAE[hougaku], GAZOU_ICHI[hougaku][0], GAZOU_ICHI[hougaku][1], GAZOU_ICHI[hougaku][2], GAZOU_ICHI[hougaku][3]);
         }
     }
 }
